86b33b6e722578cb3b8d56c70b4010ce6609b4ee
Evan <evanhaque1@gmail.com>
2024-04-18 00:12:39 -0400

    update pictures

--- src/main.jsx
+++ src/main.jsx
@@ -16,13 +16,13 @@ import Update from './update'
 const supabase = createClient( supabaseurl, api_key)
 
 const among = {
-  red: "src/sus/61d183263a856e0004c6334a.png",
-  blue: "src/sus/among_us_player_blue_icon_156941.png",
-  green: "src/sus/61d183753a856e0004c6334c.png",
-  yellow: "src/sus/Yellow.webp",
-  purple: "src/sus/9kvk25sh2sn51.png",
-  orange: "src/sus/ee0b8322db7fb7939469a67e889318d4.png",
-  pink: "src/sus/e51f80397c0915b1a688a25d52e82648.png"
+  red: "public/sus/61d183263a856e0004c6334a.png",
+  blue: "public/sus/among_us_player_blue_icon_156941.png",
+  green: "public/sus/61d183753a856e0004c6334c.png",
+  yellow: "public/sus/Yellow.webp",
+  purple: "public/sus/9kvk25sh2sn51.png",
+  orange: "public/sus/ee0b8322db7fb7939469a67e889318d4.png",
+  pink: "public/sus/e51f80397c0915b1a688a25d52e82648.png"
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
